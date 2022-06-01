d66911760a843dc6b49e01c6fbdba894fd18b42f
Allister Beharry <allister.beharry@gmail.com>
2022-06-01 11:38:08 -0400

    Add Control project.

---
+++ src/DokanS3FS.Control/Form1.cs
@@ -0,0 +1,13 @@
+using System.Windows.Forms;
+
+
+namespace DokanS3FS.Control.GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
